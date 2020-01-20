32c50f229eb96eb45591dc8bc7c07467a4c0593c
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-01-20 13:05:52 +0300

    [+] Selection Sort, written in Kotlin

--- algorithms.kts
+++ algorithms.kts
@@ -20,6 +20,32 @@ fun insertionSort(array: Array<Int>, descendingOrder: Boolean = false): Array<In
     return array
 }
 
+fun selectionSort(array: Array<Int>, descending_order: Boolean = false): Array<Int> {
+    for (i in 0..array.size - 1) {
+        var m = i
+        var j = i + 1
+        if (descending_order) {
+            while (j < array.size) {
+                if (array[j] > array[m]) {
+                    m = j
+                }
+                j = j + 1
+            }
+        } else {
+            while (j < array.size) {
+                if (array[j] < array[m]) {
+                    m = j
+                }
+                j = j + 1
+            }
+        }
+        val buffer = array[i]
+        array[i] = array[m]
+        array[m] = buffer
+    }
+    return array
+}
+
 fun customToString(array: Array<Int>): String {
     var string = ""
     for (i in array) {
@@ -34,4 +60,12 @@ fun customToString(array: Array<Int>): String {
 
 var array = arrayOf(5, 3, 4, 2, 1, 2, 3, 5, 2)
 
+println("Testing insertion sort")
+println("______________________")
 println(customToString(insertionSort(array)))
+println("______________________")
+
+println("Testing selection sort")
+println("______________________")
+println(customToString(selectionSort(array)))
+println("______________________")
