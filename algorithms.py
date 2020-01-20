3d6ca00f0ee1cb8eb062412aad18ffaa22226eb6
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-01-20 12:30:22 +0300

    [+] Selection Sort, written in Python

--- algorithms.py
+++ algorithms.py
@@ -15,6 +15,34 @@ def insertion_sort(array, descending_order=False):
     return array
 
 
+def selection_sort(array, descending_order=False):
+    for i in range(len(array) - 1):
+        m = i
+        j = i + 1
+        if descending_order:
+            while j < len(array):
+                if array[j] > array[m]:
+                    m = j
+                j = j + 1
+        else:
+            while j < len(array):
+                if array[j] < array[m]:
+                    m = j
+                j = j + 1
+        array[i], array[m] = array[m], array[i]
+    return array
+
+
+print("Testing insertion sort")
+print("______________________")
 print(insertion_sort([5, 3, 4, 2, 1, 2, 3, 5, 2], True))
 print(insertion_sort([1, 2, 4, 2, 2, 1, 5, 6, 6, 9]))
 print(insertion_sort(["a", "b", "d", "a", "h", "a"]))
+print("______________________")
+
+print("Testing selection sort")
+print("______________________")
+print(selection_sort([5, 3, 4, 2, 1, 2, 3, 5, 2], True))
+print(selection_sort([1, 2, 4, 2, 2, 1, 5, 6, 6, 9]))
+print(selection_sort(["a", "b", "d", "a", "h", "a"]))
+print("______________________")
