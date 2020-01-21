80d6429397dfee8535b3b5ee81e4768115c22942
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-01-21 16:33:57 +0300

    [+] Merge Sort, written in Kotlin

--- algorithms.kts
+++ algorithms.kts
@@ -46,6 +46,47 @@ fun selectionSort(array: Array<Int>, descending_order: Boolean = false): Array<I
     return array
 }
 
+fun mergeSort(list: List<Int>): List<Int> {
+    if (list.size <= 1) {
+        return list
+    }
+
+    val middle = list.size / 2
+    val left = list.subList(0, middle)
+    val right = list.subList(middle, list.size)
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+
+fun merge(left: List<Int>, right: List<Int>): List<Int> {
+    var indexLeft = 0
+    var indexRight = 0
+    val newList: MutableList<Int> = mutableListOf()
+
+    while (indexLeft < left.count() && indexRight < right.count()) {
+        if (left[indexLeft] <= right[indexRight]) {
+            newList.add(left[indexLeft])
+            indexLeft++
+        } else {
+            newList.add(right[indexRight])
+            indexRight++
+        }
+    }
+
+    while (indexLeft < left.size) {
+        newList.add(left[indexLeft])
+        indexLeft++
+    }
+
+    while (indexRight < right.size) {
+        newList.add(right[indexRight])
+        indexRight++
+    }
+
+    return newList
+}
+
 fun customToString(array: Array<Int>): String {
     var string = ""
     for (i in array) {
@@ -60,15 +101,23 @@ fun customToString(array: Array<Int>): String {
 
 var firstTestArray = arrayOf(5, 3, 4, 2, 1, 2, 3, 5, 2)
 var secondTestArray = arrayOf(1, 2, 4, 2, 2, 1, 5, 6, 6, 9)
+var firstTestList = mutableListOf(5, 3, 4, 2, 1, 2, 3, 5, 2)
+var secondTestList = mutableListOf(1, 2, 4, 2, 2, 1, 5, 6, 6, 9)
 
 println("Testing insertion sort")
 println("______________________")
 println(customToString(insertionSort(firstTestArray, true)))
-println(customToString(insertionSort(firstTestArray)))
+println(customToString(insertionSort(secondTestArray)))
 println("______________________")
 
 println("Testing selection sort")
 println("______________________")
 println(customToString(selectionSort(firstTestArray, true)))
-println(customToString(selectionSort(firstTestArray)))
+println(customToString(selectionSort(secondTestArray)))
+println("______________________")
+
+println("Testing merge sort")
+println("______________________")
+println(mergeSort(firstTestList))
+println(mergeSort(secondTestList))
 println("______________________")
