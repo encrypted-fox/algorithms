402f1faee9648e9723a8a069dea006334165d1d2
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-01-21 15:19:23 +0300

    [+] Merge Sort, written in Python

--- algorithms.py
+++ algorithms.py
@@ -33,11 +33,47 @@ def selection_sort(array, descending_order=False):
     return array
 
 
+def merge_sort(array, descending_order=False):
+    if len(array) > 1:
+        mid = len(array) // 2
+        left_array = array[:mid]
+        right_array = array[mid:]
+
+        merge_sort(left_array)
+        merge_sort(right_array)
+
+        i = j = k = 0
+
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                array[k] = left_array[i]
+                i += 1
+            else:
+                array[k] = right_array[j]
+                j += 1
+            k += 1
+
+        while i < len(left_array):
+            array[k] = left_array[i]
+            i += 1
+            k += 1
+
+        while j < len(right_array):
+            array[k] = right_array[j]
+            j += 1
+            k += 1
+    if descending_order:
+        return list(reversed(array))
+    else:
+        return array
+
+
 print("Testing insertion sort")
 print("______________________")
 print(insertion_sort([5, 3, 4, 2, 1, 2, 3, 5, 2], True))
 print(insertion_sort([1, 2, 4, 2, 2, 1, 5, 6, 6, 9]))
 print(insertion_sort(["a", "b", "d", "a", "h", "a"]))
+print(insertion_sort(["a", "b", "d", "a", "h", "a"], True))
 print("______________________")
 
 print("Testing selection sort")
@@ -45,4 +81,13 @@ print("______________________")
 print(selection_sort([5, 3, 4, 2, 1, 2, 3, 5, 2], True))
 print(selection_sort([1, 2, 4, 2, 2, 1, 5, 6, 6, 9]))
 print(selection_sort(["a", "b", "d", "a", "h", "a"]))
+print(selection_sort(["a", "b", "d", "a", "h", "a"], True))
+print("______________________")
+
+print("Testing merge sort")
+print("______________________")
+print(merge_sort([5, 3, 4, 2, 1, 2, 3, 5, 2], True))
+print(merge_sort([1, 2, 4, 2, 2, 1, 5, 6, 6, 9]))
+print(merge_sort(["a", "b", "d", "a", "h", "a"]))
+print(merge_sort(["a", "b", "d", "a", "h", "a"], True))
 print("______________________")
