6e010b9fefac8cc1fc5b115d64dd20645625b61f
Yaroslav Souchkov <yaroslav.souchkov@gmail.com>
2020-01-22 12:59:54 +0300

    [+] Function merge for Merge Sort moved to mergeSort function in Kotlin

--- algorithms.kts
+++ algorithms.kts
@@ -47,6 +47,34 @@ fun selectionSort(array: Array<Int>, descendingOrder: Boolean = false): Array<In
 }
 
 fun mergeSort(list: List<Int>): List<Int> {
+    fun merge(left: List<Int>, right: List<Int>): List<Int> {
+        var indexLeft = 0
+        var indexRight = 0
+        val newList: MutableList<Int> = mutableListOf()
+
+        while (indexLeft < left.count() && indexRight < right.count()) {
+            if (left[indexLeft] <= right[indexRight]) {
+                newList.add(left[indexLeft])
+                indexLeft++
+            } else {
+                newList.add(right[indexRight])
+                indexRight++
+            }
+        }
+
+        while (indexLeft < left.size) {
+            newList.add(left[indexLeft])
+            indexLeft++
+        }
+
+        while (indexRight < right.size) {
+            newList.add(right[indexRight])
+            indexRight++
+        }
+
+        return newList
+    }
+
     if (list.size <= 1) {
         return list
     }
@@ -56,35 +84,6 @@ fun mergeSort(list: List<Int>): List<Int> {
     val right = list.subList(middle, list.size)
 
     return merge(mergeSort(left), mergeSort(right))
-}
-
-
-fun merge(left: List<Int>, right: List<Int>): List<Int> {
-    var indexLeft = 0
-    var indexRight = 0
-    val newList: MutableList<Int> = mutableListOf()
-
-    while (indexLeft < left.count() && indexRight < right.count()) {
-        if (left[indexLeft] <= right[indexRight]) {
-            newList.add(left[indexLeft])
-            indexLeft++
-        } else {
-            newList.add(right[indexRight])
-            indexRight++
-        }
-    }
-
-    while (indexLeft < left.size) {
-        newList.add(left[indexLeft])
-        indexLeft++
-    }
-
-    while (indexRight < right.size) {
-        newList.add(right[indexRight])
-        indexRight++
-    }
-
-    return newList
 }
 
 fun customToString(array: Array<Int>): String {
